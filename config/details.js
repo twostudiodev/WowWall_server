f9cbcbf70dd734ea05aec4b040dc1bf76afe54c5
Hammad-4846 <curious4846@gmail.com>
2025-03-23 16:40:18 +0530

    Added new Email add

--- config/details.js
+++ config/details.js
@@ -1,5 +1,5 @@
 const authorizedDomains = {
-    "blue-star-fe.vercel.app": "bspkrishnawebsite@gmail.com",
+    "blue-star-fe.vercel.app": "bluestarpublication@gmail.com",
     "two12studio.com": "connect@two12studio.com",
     "localhost": "connect@two12studio.com",
 
